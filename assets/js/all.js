db041cf66834f51939cc28398ed3d12e589354a8
Alec Wang <wywsmail@gmail.com>
2020-06-24 03:45:27 +0800

    add doneTask

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,7 +4,8 @@ var taskInput = document.querySelector("#taskInput");
 var taskAdd = document.querySelector("#taskAdd");
 var taskRemoveAll = document.querySelector("#taskRemoveAll");
 var taskList = document.querySelector(".taskList");
-var deadLine = document.querySelector("#deadLine");
+var deadLine = document.querySelector("#deadLine"); // let done = document.querySelectorAll(".done");
+
 taskAdd.addEventListener("click", addTask, false);
 taskRemoveAll.addEventListener("click", removeAllTask, false);
 var data = [];
@@ -30,18 +31,29 @@ function removeTask(e) {
 function render() {
   var string = '';
   data.forEach(function (item, i) {
-    string += "\n      <tr>\n        <th scope=\"row\" class=\"text-center\">".concat(i + 1, "</th>\n        <td>").concat(item.text, "</td>\n        <td class=\"text-center\">").concat(item.deadline, "</td>\n        <td class=\"text-center\"><button class=\"taskRemove text-center border-0\">\u522A\u9664\u4EFB\u52D9</button></td>\n      </tr>");
+    string += "\n      <tr>\n        <th scope=\"row\" class=\"text-center\">".concat(i + 1, "</th>\n        <td>").concat(item.text, "</td>\n        <td class=\"text-center\">").concat(item.deadline, "</td>\n        <td class=\"text-center\">\n          <input type=\"checkbox\" class=\"done\" data-i=\"").concat(i, "\" ").concat(item.done, ">\n        </td>\n        <td class=\"text-center\"><button class=\"taskRemove text-center border-0\" data-i=\"").concat(i, "\">\u522A\u9664\u4EFB\u52D9</button></td>\n      </tr>");
   });
   taskList.innerHTML = string;
   var taskRemove = document.querySelectorAll(".taskRemove");
   taskRemove.forEach(function (item) {
     item.addEventListener("click", removeTask, false);
   });
+  var taskDone = document.querySelectorAll(".done");
+  taskDone.forEach(function (item) {
+    item.addEventListener("click", doneTask, false);
+  });
 }
 
 function removeAllTask(e) {
   e.preventDefault();
   data = [];
+  render();
+}
+
+function doneTask(e) {
+  e.preventDefault();
+  data[e.target.dataset.i].done = 'checked';
+  console.log(e.target.dataset.i);
   render();
 }
 //# sourceMappingURL=all.js.map
